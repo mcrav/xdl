0440eb40c20a8a7c6401d859e9a7e256f5b4419f
Matthew Craven <matthew.craven@glasgow.ac.uk>
2019-04-19 10:18:26 +0100

    Dissolve step can now dissolve in rotavap

--- xdl/steps/steps_synthesis/dissolve.py
+++ xdl/steps/steps_synthesis/dissolve.py
@@ -1,7 +1,20 @@
 from typing import Optional
 from ..base_step import Step
 from ..steps_utility import Wait, HeatChillToTemp
+from ..steps_base import (
+    CRotavapSetRotationSpeed,
+    CRotavapStartRotation,
+    CRotavapStopRotation,
+    CRotavapSetTemp,
+    CRotavapStartHeater,
+    CRotavapStopHeater,
+    CRotavapLiftDown,
+    CRotavapLiftUp,
+    CStopVacuum,
+    CVentVacuum
+)
 from .add import Add
+from ...constants import DEFAULT_DISSOLVE_ROTAVAP_ROTATION_SPEED, EVAPORATE_PORT
 
 class Dissolve(Step):
     """Dissolve contents of vessel in given solvent.
@@ -14,6 +27,9 @@ class Dissolve(Step):
         temp (float): Temperature to stir at. Optional.
         time (float): Time to stir for. Optional.
         solvent_vessel (str): Given internally. Flask containing solvent.
+        vessel_is_rotavap (bool): Given internally. True is vessel is a rotavap.
+            Needed as different procedure used for dissolving a solid in a
+            rotavap.
     """
     def __init__(
         self,
@@ -25,21 +41,43 @@ class Dissolve(Step):
         time: Optional[float] = 'default',
         stir_rpm: Optional[float] = 'default',
         solvent_vessel: Optional[str] = None,
+        vessel_is_rotavap: Optional[bool] = False,
     ) -> None:
         super().__init__(locals())
 
-        self.steps = [
-            Add(reagent=self.solvent,
-                volume=self.volume,
-                vessel=self.vessel,
-                port=self.port),
-            HeatChillToTemp(
-                vessel=self.vessel,
-                temp=self.temp,
-                stir=True,
-                stir_rpm=self.stir_rpm),
-            Wait(self.time),
-        ]
+        if vessel_is_rotavap:
+            self.steps = [
+                Add(reagent=self.solvent,
+                    volume=self.volume,
+                    vessel=self.vessel,
+                    port=EVAPORATE_PORT,
+                    stir=False),
+                CRotavapSetTemp(self.vessel, self.temp),
+                CRotavapStartHeater(self.vessel),
+                CRotavapSetRotationSpeed(
+                    self.vessel, DEFAULT_DISSOLVE_ROTAVAP_ROTATION_SPEED),
+                CRotavapStartRotation(self.vessel),
+                CRotavapLiftDown(self.vessel),
+
+                Wait(self.time),
+
+                CRotavapStopRotation(self.vessel),
+                CRotavapStopHeater(self.vessel),
+                CRotavapLiftUp(self.vessel),
+            ]
+        else:
+            self.steps = [
+                Add(reagent=self.solvent,
+                    volume=self.volume,
+                    vessel=self.vessel,
+                    port=self.port),
+                HeatChillToTemp(
+                    vessel=self.vessel,
+                    temp=self.temp,
+                    stir=True,
+                    stir_rpm=self.stir_rpm),
+                Wait(self.time),
+            ]
 
         self.human_readable = f'Dissolve contents of {vessel} in {solvent} ({volume} mL) at {temp} °C over {time} seconds.'
 
