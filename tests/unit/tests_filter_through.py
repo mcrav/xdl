5ea90b4c8a6a6081ab3ccee86090d08eb3517de4
Matthew Craven <matthew.craven@glasgow.ac.uk>
2019-04-24 12:21:30 +0100

    Fixed bugs with FilterThrough step and added test

--- tests/unit/tests_filter_through.py
+++ tests/unit/tests_filter_through.py
@@ -4,8 +4,8 @@ from ..utils import generic_chempiler_test
 HERE = os.path.abspath(os.path.dirname(__file__))
 FOLDER = os.path.join(HERE, 'files')
 
-def test_clean_vessel():
+def test_filter_through():
     """Test that dissolving a solid in the rotavap works."""
-    xdl_f = os.path.join(FOLDER, 'clean_reactor_rotavap.xdl')
+    xdl_f = os.path.join(FOLDER, 'filter_through.xdl')
     graph_f = os.path.join(FOLDER, 'rotavap_reactor_cartridge.json')
     generic_chempiler_test(xdl_f, graph_f)
--- tests/unit/tests_rotavap.py
+++ tests/unit/tests_rotavap.py
@@ -7,17 +7,17 @@ FOLDER = os.path.join(HERE, 'files')
 def test_dissolve_in_rotavap():
     """Test that dissolving a solid in the rotavap works."""
     xdl_f = os.path.join(FOLDER, 'dissolve_in_rotavap.xdl')
-    graph_f = os.path.join(FOLDER, 'rotavap_reactor.json')
+    graph_f = os.path.join(FOLDER, 'rotavap_reactor_cartridge.json')
     generic_chempiler_test(xdl_f, graph_f)
 
 def test_wash_in_rotavap():
     """Test that washing a solid in the rotavap works."""
     xdl_f = os.path.join(FOLDER, 'wash_in_rotavap.xdl')
-    graph_f = os.path.join(FOLDER, 'rotavap_reactor.json')
+    graph_f = os.path.join(FOLDER, 'rotavap_reactor_cartridge.json')
     generic_chempiler_test(xdl_f, graph_f)
 
 def test_rotavap():
     """Test rotavap evaporate step."""
     xdl_f = os.path.join(FOLDER, 'rotavap.xdl')
-    graph_f = os.path.join(FOLDER, 'rotavap_reactor.json')
+    graph_f = os.path.join(FOLDER, 'rotavap_reactor_cartridge.json')
     generic_chempiler_test(xdl_f, graph_f)
