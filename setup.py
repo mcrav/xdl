f3be278b4f785e140a7061ad4de04cd4393e76a6
Matthew Craven <matthew.craven@glasgow.ac.uk>
2019-07-04 17:25:33 +0100

    Fixed bug in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(name='xdl',
-      version='0.1.0',
-      description='Package for working with XDL (chemical descriptive language).',
-      author='Matthew Craven',
-      author_email='matthew.craven@glasgow.ac.uk',
-      packages=find_packages(),
-      package_data={'xdl': ['localisation/xdl_translations.txt']},
-      include_package_data=True,
+setup(
+    name='xdl',
+    version='0.1.0',
+    description='Package for working with XDL (chemical descriptive language).',
+    author='Matthew Craven',
+    author_email='matthew.craven@glasgow.ac.uk',
+    packages=find_packages(),
+    package_data={
+        'xdl': [
+            'localisation/xdl_translations.txt',
+            'graphgen/chemputer_std6.json',
+        ]
+    },
+    include_package_data=True,
 )
