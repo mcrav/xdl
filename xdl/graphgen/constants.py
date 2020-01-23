cc8738aea4f37ea4a5dadb2e3085e1b842589030
Matthew Craven <matt723@hotmail.co.uk>
2020-01-23 10:13:10 +0000

    Added graphgen flake8 to CI

--- xdl/graphgen/constants.py
+++ xdl/graphgen/constants.py
@@ -1,7 +1,9 @@
 HEATER_CHILLER_TEMP_RANGES = {
     'JULABOCF41': (-40, 100),
-    'IKARV10': (18, 180),  # https://www.wilmad-labglass.com/uploadedFiles/Main_Site/Content/Literature_Center/RV10_WEB.pdf
-    'IKARCTDigital': (18, 360), # https://www.ika.com/en/Products-Lab-Eq/Magnetic-Stirrers-Hot-Plate-Lab-Mixer-Stirrer-Blender-csp-188/RCT-basic-cpdt-3810000/
+    # https://www.wilmad-labglass.com/uploadedFiles/Main_Site/Content/Literature_Center/RV10_WEB.pdf # noqa: E501
+    'IKARV10': (18, 180),
+    # https://www.ika.com/en/Products-Lab-Eq/Magnetic-Stirrers-Hot-Plate-Lab-Mixer-Stirrer-Blender-csp-188/RCT-basic-cpdt-3810000/ # noqa: E501
+    'IKARCTDigital': (18, 360),
     'Huber': (-40, 100),
 }
 
@@ -18,5 +20,5 @@ MISSING_COMPONENT_TYPE_ERROR = 'missing_component_type_error'
 MISSING_HEATER_OR_CHILLER_ERROR = 'missing_heater_or_chiller_error'
 CANNOT_REACH_TARGET_TEMP_ERROR = 'cannot_reach_target_temp_error'
 
-DEFAULT_BUFFER_FLASK_VOLUME: int = 500 # mL
-DEFAULT_REAGENT_FLASK_VOLUME: int = 100 # mL
+DEFAULT_BUFFER_FLASK_VOLUME: int = 500  # mL
+DEFAULT_REAGENT_FLASK_VOLUME: int = 100  # mL
