047f332e5b4e29ed3a392dba4df8fbbc1cdd0324
Matthew Craven <matt723@hotmail.co.uk>
2020-02-12 18:15:54 +0000

    Updated XDL generator for compatibility with new default props

--- xdl/utils/sanitisation.py
+++ xdl/utils/sanitisation.py
@@ -155,6 +155,9 @@ def convert_val_to_std_units(val: str) -> float:
     Returns:
         float: Value in default units.
     """
+    if type(val) != str:
+        return val
+
     float_regex_pattern = r'([-]?[0-9]+(?:[.][0-9]+)?)'
     unit_search = re.search(r'[a-zA-Zμ°]+[3]?', val)
     val_search = re.search(float_regex_pattern, val)
