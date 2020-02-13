475d9545a14098abd3803e42e9687f0de2a3a8a7
Matthew Craven <matt723@hotmail.co.uk>
2020-02-13 12:00:20 +0000

    Bumped version number

--- xdl/constants.py
+++ xdl/constants.py
@@ -2,7 +2,7 @@ from typing import Dict, List
 
 #: XDL version number. Remember to increment after merging into master. Used
 # in header at top of outputted XDL files.
-XDL_VERSION: str = '0.2.1'
+XDL_VERSION: str = '0.3.0'
 
 ######################
 # DEFAULT PARAMETERS #
