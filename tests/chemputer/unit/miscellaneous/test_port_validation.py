5b7ccc017c728b7ec3e3bec1bab7b022265dc51e
Graham Keenan <graham.keenan@outlook.com>
2020-02-17 18:25:00 +0000

    Issue #173: Refactored test structure

---
+++ tests/chemputer/unit/miscellaneous/test_port_validation.py
@@ -0,0 +1,61 @@
+from xdl.platforms.chemputer.executor import ChemputerExecutor
+from xdl.steps import Add, Transfer
+from xdl.utils.errors import IllegalPortError
+import pytest
+import os
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+FOLDER = os.path.join(HERE, '..', 'files')
+
+@pytest.mark.unit
+def test_port_validation():
+    executor = ChemputerExecutor()
+    bigrig = os.path.join(FOLDER, 'bigrig.json')
+    with pytest.raises(IllegalPortError):
+        block = [Add(reagent="water", vessel="reactor", volume=5, port="top")]
+        executor.prepare_block_for_execution(bigrig, block)
+
+    block = [Add(reagent="water", vessel="reactor", volume=5, port="0")]
+    executor.prepare_block_for_execution(bigrig, block)
+
+    with pytest.raises(IllegalPortError):
+        block = [Transfer(
+            from_vessel="rotavap",
+            to_vessel="reactor",
+            volume=5,
+            from_port="top")]
+        executor.prepare_block_for_execution(bigrig, block)
+
+    block = [Transfer(
+        from_vessel="rotavap",
+        to_vessel="reactor",
+        volume=5,
+        from_port="evaporate")]
+    executor.prepare_block_for_execution(bigrig, block)
+    block = [Transfer(
+        from_vessel="rotavap",
+        to_vessel="reactor",
+        volume=5,
+        from_port="collect")]
+    executor.prepare_block_for_execution(bigrig, block)
+
+    with pytest.raises(IllegalPortError):
+        block = [Transfer(
+            from_vessel="rotavap",
+            to_vessel="filter",
+            volume=5,
+            to_port="0")]
+        executor.prepare_block_for_execution(bigrig, block)
+
+    block = [Transfer(
+        from_vessel="rotavap",
+        to_vessel="filter",
+        volume=5,
+        to_port="top")]
+    executor.prepare_block_for_execution(bigrig, block)
+    block = [Transfer(
+        from_vessel="rotavap",
+        to_vessel="filter",
+        volume=5,
+        to_port="bottom")]
+    executor.prepare_block_for_execution(bigrig, block)
--- xdl/step_utils/special_steps.py
+++ xdl/step_utils/special_steps.py
@@ -483,3 +483,6 @@ class Callback(AbstractBaseStep):
 
     def execute(self, chempiler, logger, level=0):
         self.fn(*self.args, **self.keyword_args)
+
+    def locks(self, chempiler):
+        raise NotImplementedError(f"Function `locks` not implemented!")
