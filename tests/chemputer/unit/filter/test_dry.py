d0236e6fe89d201cd2eab7c70a1113df35e149a3
Graham Keenan <graham.keenan@outlook.com>
2020-02-21 15:23:34 +0000

    Issue #112: Inert gas should now connect with flask when no vacuum device is supplied

--- tests/chemputer/unit/filter/test_dry.py
+++ tests/chemputer/unit/filter/test_dry.py
@@ -25,3 +25,21 @@ def test_dry_in_rotavap():
     xdl_f = os.path.join(FOLDER, 'dry_in_rotavap.xdl')
     graph_f = os.path.join(FOLDER, 'bigrig.json')
     generic_chempiler_test(xdl_f, graph_f)
+
+@pytest.mark.unit
+def test_dry_inert_gas_connection():
+    """Test confirms issue #112 fixed during reworking of Dry."""
+    from xdl import XDL
+    from xdl.platforms.chemputer.steps.steps_base.general import CConnect
+
+    xdl_f = os.path.join(FOLDER, 'dry.xdl')
+    graph_f = os.path.join(FOLDER, 'bigrig_no_vacuum_device.json')
+    generic_chempiler_test(xdl_f, graph_f)
+
+    x = XDL(xdl_f)
+    x.prepare_for_execution(graph_f)
+    for step in x.steps:
+        if step.name == 'Dry':
+            for substep in step.steps:
+                if type(substep) == CConnect:
+                    assert substep.from_vessel == 'flask_nitrogen'
