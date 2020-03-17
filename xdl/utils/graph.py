506f03e954d5346c3cc0df8817fe43644c0a41bb
Matthew Craven <matt723@hotmail.co.uk>
2020-03-17 13:43:42 +0000

    Fixed issue with duplicates in undirected_neighbors

--- xdl/utils/graph.py
+++ xdl/utils/graph.py
@@ -8,12 +8,14 @@ def undirected_neighbors(graph, node, data=False):
     yielded_neighbors = []
     for src, dest in graph.edges():
         if src == node and dest not in yielded_neighbors:
+            yielded_neighbors.append(dest)
             if data:
                 yield dest, graph.nodes[dest]
             else:
                 yield dest
 
         elif dest == node and src not in yielded_neighbors:
+            yielded_neighbors.append(src)
             if data:
                 yield src, graph.nodes[src]
             else:
