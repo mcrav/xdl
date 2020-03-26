6c51590bc0d153a384cfe560ba8d40af2573ce15
Matthew Craven <mcrav@protonmail.com>
2020-03-26 16:53:06 +0000

    Bumped version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='xdl',
-    version='0.4.0',
+    version='0.4.2',
     description='Package for working with XDL (chemical descriptive language).',
     author='Matthew Craven',
     author_email='matthew.craven@glasgow.ac.uk',
