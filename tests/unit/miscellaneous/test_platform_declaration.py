f082ad46a9a0a44e0b77fb7bcc59ecf9c9fd4459
Matthew Craven <mcrav@protonmail.com>
2021-01-11 12:01:50 +0000

    Fixed bug with saving platform declaration to JSON and added test

--- tests/unit/miscellaneous/test_platform_declaration.py
+++ tests/unit/miscellaneous/test_platform_declaration.py
@@ -1,7 +1,10 @@
 import pytest
+import json
 from xdl.platforms.placeholder import PlaceholderPlatform
 
 @pytest.mark.unit
 def test_platform_declaration():
-    """Test platform declaration generation runs without error."""
-    return PlaceholderPlatform().declaration
+    """Test platform declaration generation runs without error, and can be saved
+    to JSON file (ChemifyAPI needs this functionality).
+    """
+    return json.dumps(PlaceholderPlatform().declaration)
